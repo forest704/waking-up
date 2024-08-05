a12e204b177053eb61bc7314843eca8a07f98d35
forest704 <altforowban5@gmail.com>
2024-08-05 15:53:32 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "bgm":return hex`0078000408040205001c000f0a006400f4010a0000040000000000000000000000000000000002600000000800012708001000011d10001800011e18002000011b20002800012528003000011b30003800011d38004000011940004800012748005000011d50005800011e58006000011b60006800012568007000011b70007800011d78008000011906001c00010a006400f4016400000400000000000000000000000000000000024e000000040002182c04000800011810001400020a2c2000240002162c24002800011630003400012c4000440002182c44004800011850005400020a2c6000640002162c64006800011670007400012c`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
